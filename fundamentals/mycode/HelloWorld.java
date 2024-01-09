93032d848149def20641b97559481e1f474d4088
Tiyani Baloyi <baloyitiyani22@gmail.com>
2024-01-09 07:27:39 +0200

    daily progress

---
+++ fundamentals/mycode/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args){
+	    System.out.print("This message is the first step to fundamentals of the Java_bootcamp");
+		
+		}
+}
---
+++ fundamentals/mycode/Rule1.java
@@ -0,0 +1,7 @@
+public class Rule1 {
+    public static void main(String[] args) {
+        int Grade1_count = 98;
+        int Grade2_count = 88;
+        System.out.print("Sum of Grade One and Two is " + (Grade1_count + Grade2_count));
+    }
+}
