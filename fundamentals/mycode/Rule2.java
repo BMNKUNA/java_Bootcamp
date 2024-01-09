9b271ff463afd09a98785c64f2d0e54448e1e118
Monde <bmwnkuna@gmail.com>
2024-01-09 16:00:59 +0200

    day 2 bootcamp work

---
+++ fundamentals/mycode/Rule2.java
@@ -0,0 +1,7 @@
+public class Rule2 {
+    public static void main(String[] arg){
+        int _salary = 2000;
+        int _deductions = 1800;
+        System.out.print("residual funds are" + " " + (_salary - _deductions)); 
+        }
+}
---
+++ fundamentals/mycode/StringTest1.java
@@ -0,0 +1,7 @@
+public class StringTest1{
+	public static void main(String[] args){
+	String Name = "Monde";
+	String Surname = "Nkuna";
+	    System.out.print("FullName " + Name +" "+ Surname);
+	}    
+}
